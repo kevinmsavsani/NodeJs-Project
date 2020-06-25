70d9f1779b55f4011e83ef14fa6e95f9a3cafc72
kevinmsavsani <kevinmsavsani@gmail.com>
2020-06-25 19:32:34 +0530

    post added - data getting form

--- Node.js
+++ Node.js
@@ -8,12 +8,22 @@ var app = express();
 const cities = require('cities');
 const url = require('url');
 const http = require('http');
+var parser = require('body-parser');
+var path = require('path');
+var city, query;
+
+app.use(parser.urlencoded({ extended: false }))
+app.use(parser.json())
+ 
+app.use(function(req,res,next){
+    res.locals.userValue = null;
+    next();
+})
   
 // Set EJS as templating engine 
-app.set('view engine', 'ejs'); 
+app.set('view engine','ejs');
 
 app.get('/', (req, res)=>{ 
-  var city, query;
   query = url.parse(req.url, true).query;
   if (query.zipCode) city = cities.zip_lookup(query.zipCode).city;
   else city = "not found"
@@ -22,12 +32,32 @@ app.get('/', (req, res)=>{
 // page to be rendered as input. 
 // This page should be in views folder  
 // in the root directory. 
-  
-res.render('home', {city:city}); 
-}); 
+
+    res.render('home',{
+        topicHead : 'Student Form',city:city
+    });
+    console.log('user accessing Home page');
+});
+
+app.post('/student/add',function(req,res){
+    var student = {
+        first : req.body.fname,
+        last : req.body.lname
+    }
+    console.log(student);
+    res.render('home',{
+        userValue : student,
+        topicHead : 'Student Form',
+        city:city
+    });
+    //res.json(student);
+     
+});
+
   
 var server = app.listen(4000, function(){ 
     console.log('listining to port 4000') 
 }); 
+
 
 
